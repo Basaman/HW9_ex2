04d591f978f4f6ab4574a1b73003c0a7e9215791
ilya <ilia.arkhangelsky@gmail.com>
2023-02-01 17:04:15 +0300

    сумма натуральных чисел в промежутке от M до N

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму 
+//натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 120
+//M = 4; N = 8. -> 30
+
+Console.Write("Введите целое число M: ");
+int numberM = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите целое число N: ");
+int numberN = int.Parse(Console.ReadLine() ?? "");
+
+int sumNumbers = GetSumNumbers(numberM, numberN);
+Console.Write($"M = {numberM}; N = {numberN}; -> {sumNumbers}");
+
+int GetSumNumbers(int startNumber, int lastNumber)
+{
+    int sum = 0;
+    if (startNumber <= lastNumber)
+    {
+        sum = startNumber + GetSumNumbers(startNumber + 1, lastNumber);
+    }
+    return sum;
+}
